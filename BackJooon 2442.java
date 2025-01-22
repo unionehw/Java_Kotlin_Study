7fdd7a58edde60082f00f7653403284643766896
HeeWon Youn <127981714+unionehw@users.noreply.github.com>
2025-01-22 13:42:09 +0900

    BackJooon 2442.java

--- BackJooon 2442.java
+++ BackJooon 2442.java
@@ -6,11 +6,11 @@ public class Main {
 
     int N = scanner.nextInt(); // N 입력받기
 
-    // 1부터 N까지 반복               
-    for (int i = 1; i <=N; i++) {                 //현재 줄 번호(i)
+    // 1번째 줄부터 N번째 줄까지 출력       
+    for (int i = 1; i <=N; i++) {                 
     // (N - i)개의 공백 출력
-      for(int j = 1; j<=N - i; j++) [             //현재 줄의 공백 처리(n-i)
-        System.out.print(" ");
+      for(int j = 1; j<=N - i; j++) [           
+        System.out.print(" ");                    // 공백 출력
     }
       // (2 * i -`)개의 별 출력
       for (int k =1; k<=2 * i-1; k++) {           // 별의 갯수(2n-1)
@@ -22,6 +22,6 @@ public class Main {
   }
 }
 
-
+N: 총 줄의 갯수
 i번째 줄에 공백의 개수: N - i
 i번째 줄에 별의 개수: 2 x i - 1
